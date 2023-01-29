da2a18d868b293665435ec34cbc450094e7880e5
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-01-29 17:56:05 +0700

    Lint fix

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,13 +1,9 @@
 from app import create_app, reset_db
 from app.models.vending_machine import Machine
 
-def test_create_app():
+
+def test_create_app() -> None:
     app = create_app()
     reset_db(app=app)
     all_machine_instances_count = len(Machine.query.all())
     assert all_machine_instances_count == 0
-
-    
-
-
-
